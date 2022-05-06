6d961eb3844e196ace7efbdd83fbeedc5486db9e
DESKTOP-V07OTS6\panka <pankaj.pandey@talent-cabin.com>
2022-05-06 23:17:08 +0530

    Duplicate number iin array

---
+++ src/SituationBased/DuplicateNuminArray.java
@@ -0,0 +1,18 @@
+package SituationBased;
+
+//How to find the duplicate number on a given integer array?
+public class DuplicateNuminArray {
+
+	public static void main(String[] args) {
+		int [] arr = new int [] {1, 2, 3, 4, 2, 7, 8, 8, 3};    
+        //Searches for duplicate element  
+        for(int i = 0; i < arr.length; i++) {  
+            for(int j = i + 1; j < arr.length; j++) {  
+                if(arr[i] == arr[j])  
+                    System.out.println(arr[j]);  
+            }  
+        }  
+
+	}
+
+}
