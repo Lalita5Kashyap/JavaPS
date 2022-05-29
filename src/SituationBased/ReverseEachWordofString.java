e89f7f3b1dc0a7ce75d10650bee10a08638b81c8
DESKTOP-V07OTS6\panka <pankaj.pandey@talent-cabin.com>
2022-05-29 12:26:51 +0530

    Everything updated

---
+++ src/SituationBased/ReverseEachWordofString.java
@@ -0,0 +1,25 @@
+package SituationBased;
+
+
+public class ReverseEachWordofString {
+		  public static void main(String[] args) {
+		   String originalStr= "Unresolved compilation problem";
+		 
+		    String words[] = originalStr.split("\\s");
+		    String reversedString = "";
+		 
+		    for (int i = 0; i < words.length; i++) 
+		    {
+		      String word = words[i];
+		      String reverseWord = "";
+		      for (int j = word.length() - 1; j >= 0; j--) {
+		        reverseWord = reverseWord + word.charAt(j);
+		      }
+		      reversedString = reversedString + reverseWord + " ";
+		    }
+		 
+		    // Displaying the string after reverse
+		    System.out.print("Reversed string : " + reversedString);
+		    
+		  }
+		}
